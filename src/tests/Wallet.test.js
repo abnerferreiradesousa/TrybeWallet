8b73c0beb44e06b953c2fdbfae168d65ee8fdb1d
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-05-13 14:06:56 -0300

    [feat]: tests fronted completed

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { screen, fireEvent, waitFor } from '@testing-library/react';
+import { screen, fireEvent, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import App from '../App';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 import { stateWithExpenses, stateWithEmail, responseCoins } from './helpers/mockData';
 import Wallet from '../pages/Wallet';
 import userEvent from '@testing-library/user-event';
 
-
 import {getRoles, logRoles, within} from '@testing-library/dom'
 
 const INPUT_EMAIL_TEST_ID = 'email-input';
 const TOTAL_FIELD_TEST_ID = 'total-field';
 const CURRENCY_EXCHANGE_FIELD = 'header-currency-field';
+const EDIT_INPUT_TEST_ID = 'edit-btn';
+const VALUE_INPUT_TEST_ID = 'value-input';
+const CURRENCY_INPUT_TEST_ID = 'currency-input';
+const METHOD_INPUT_TEST_ID = 'method-input';
+const TAG_INPUT_TEST_ID = 'tag-input';
+const DESCRIPTION_INPUT_TEST_ID = 'description-input';
+const BTN_EDIT_TEST_ID = 'edit-btn';
 
 const responseFromAPI = Promise.resolve({
   json: () => Promise.resolve(responseCoins),
@@ -199,7 +205,7 @@ describe('A página wallet', () => {
     });
    describe('Deve renderizar dentro de options as siglas de moedas buscando de uma API', () => {
     test('12 - renderiza as siglas corretamente', async () => {
-      renderWithRouterAndRedux(<Wallet />, '/carteira');
+      renderWithRouterAndRedux(<Wallet />);
       const currencyInput = await screen.findByRole('combobox', {
         name: /moeda/i,
       });
@@ -219,4 +225,111 @@ describe('A página wallet', () => {
       expect(currencyInput).toBeInTheDocument();
     });
    });
+   describe('Deve renderizar um header de tabela com os campos', () => {
+  
+    test('14 - Descrição, Tag, Método de pagamento, Valor, Moeda, Câmbio utilizado, Valor convertido e Moeda de conversão', () => {
+      renderWithRouterAndRedux(<Wallet />);
+  
+      expect(screen.getByRole('columnheader', { name: 'Descrição' })).toBeInTheDocument();
+      expect(screen.getByRole('columnheader', { name: 'Tag' })).toBeInTheDocument();
+      expect(screen.getByRole('columnheader', { name: 'Método de pagamento' })).toBeInTheDocument();
+      expect(screen.getByRole('columnheader', { name: 'Moeda' })).toBeInTheDocument();
+      expect(screen.getByRole('columnheader', { name: 'Câmbio utilizado' })).toBeInTheDocument();
+      expect(screen.getByRole('columnheader', { name: 'Valor convertido' })).toBeInTheDocument();
+      expect(screen.getByRole('columnheader', { name: 'Moeda de conversão' })).toBeInTheDocument();
+      expect(screen.getByRole('columnheader', { name: 'Editar/Excluir' })).toBeInTheDocument();
+    });
+  });
+  describe('Deve renderizar um corpo com os campos', () => {
+    test('15 - Descrição, Tag, Método de pagamento, Valor, Moeda, Câmbio utilizado, Valor convertido e Moeda de conversão', () => {
+      renderWithRouterAndRedux(<Wallet />, {
+        initialEntries: ['/carteira'], initialState: stateWithExpenses
+      });
+      expect(screen.getAllByRole('cell', { name: 'Abajur' })[0]).toBeInTheDocument();
+      expect(screen.getAllByRole('cell', { name: 'Trabalho' })[0]).toBeInTheDocument();
+      expect(screen.getAllByRole('cell', { name: 'Cartão de crédito' })[0]).toBeInTheDocument();
+      expect(screen.getAllByRole('cell', { name: '1000.00' })[0]).toBeInTheDocument();
+      expect(screen.getAllByRole('cell', { name: 'Libra Esterlina/Real Brasileiro' })[0]).toBeInTheDocument();
+      expect(screen.getAllByRole('cell', { name: '6.24' })[0]).toBeInTheDocument();
+      expect(screen.getAllByRole('cell', { name: '6236.10' })[0]).toBeInTheDocument();
+      expect(screen.getAllByRole('cell', { name: 'Real' })[0]).toBeInTheDocument();
+    });
+  });
+
+describe('Deve ser possível editar uma despesa já cadastrada', () => {
+
+  test('16 - O botão deve estar dentro do último item da linha da tabela e deve possuir `data-testid="edit-btn"`', () => {
+    renderWithRouterAndRedux(<Wallet />, {
+      initialEntries:['/carteira'], initialState: stateWithExpenses
+    });
+    const btnEdit = document.getElementsByTagName('tr')[1].lastChild.firstChild;
+    expect(btnEdit).toBeInTheDocument();
+    expect(btnEdit.dataset.testid).toBe(BTN_EDIT_TEST_ID);    
+});
+
+  test('17 - Ao ser clicado, o botão habilita um formulário para editar a linha da tabela. Ao clicar em "Editar despesa" ela é atualizada e atualiza a soma de despesas no header.', async () => {
+    const { store } = renderWithRouterAndRedux(<Wallet />, {
+      initialEntries:['/carteira'], initialState: stateWithExpenses
+    });
+    const toggleEditBtn = screen.getAllByTestId(EDIT_INPUT_TEST_ID)[0];
+    fireEvent.click(toggleEditBtn);
+
+    const totalField = screen.getByTestId(TOTAL_FIELD_TEST_ID);
+    const valueInput = await screen.findByTestId(VALUE_INPUT_TEST_ID);
+    const currencyInput = await screen.findByTestId(CURRENCY_INPUT_TEST_ID);
+    const methodInput = await screen.findByTestId(METHOD_INPUT_TEST_ID);
+    const tagInput = await screen.findByTestId(TAG_INPUT_TEST_ID);
+    const descriptionInput = await screen.findByTestId(DESCRIPTION_INPUT_TEST_ID);
+    const editButton = await screen.findByText(/Editar despesa/i);
+
+    userEvent.clear(valueInput)
+    userEvent.clear(descriptionInput)
+
+    userEvent.type(valueInput, '100');
+    userEvent.selectOptions(currencyInput, 'CAD');
+    userEvent.selectOptions(methodInput, 'Dinheiro');
+    userEvent.selectOptions(tagInput, 'Trabalho');
+    userEvent.type(descriptionInput, 'Trampolim');
+
+    expect(totalField).toContainHTML('6236.10');
+    fireEvent.click(editButton);
+
+    // await waitFor(() => {
+    //   expect(screen.getByRole('cell', { name: 'Trampolim' })).toBeInTheDocument();
+    // });
+    
+    expect(screen.getByRole('cell', { name: 'Trampolim' })).toBeInTheDocument();
+    expect(screen.getAllByRole('cell', { name: 'Trabalho' })[0]).toBeInTheDocument();
+    expect(screen.getAllByRole('cell', { name: 'Dinheiro' })[0]).toBeInTheDocument();
+    expect(screen.getAllByRole('cell', { name: '389.51' })[0]).toBeInTheDocument();
+    expect(screen.getAllByRole('cell', { name: 'Dólar Canadense/Real Brasileiro' })[0]).toBeInTheDocument();
+    expect(screen.getAllByRole('cell', { name: '3.90' })[0]).toBeInTheDocument();
+    expect(screen.getAllByRole('cell', { name: '100.00' })[0]).toBeInTheDocument();
+    expect(screen.getAllByRole('cell', { name: 'Real' })[0]).toBeInTheDocument();
+
+    const newExpenses = [
+      {
+        id: 0,
+        value: '100',
+        currency: 'CAD',
+        method: 'Dinheiro',
+        tag: 'Trabalho',
+        description: 'Trampolim',
+        exchangeRates: responseCoins,
+      },
+      // {
+      //   id: 1,
+      //   value: '20',
+      //   currency: 'EUR',
+      //   method: 'Dinheiro',
+      //   tag: 'Trabalho',
+      //   description: 'Vinte euros',
+      //   exchangeRates: mockData,
+      // },
+    ];
+
+    expect(totalField).toContainHTML('389.51');
+    expect(store.getState().wallet.expenses).toStrictEqual(newExpenses);
+  });
+});
 });
